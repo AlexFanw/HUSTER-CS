e6bb0bd54a1ac70c6cec7f9c9ef6d1aaba2665d7
AlexFanw <alex8802732@gmail.com>
2020-05-22 23:14:39 +0800

    🚀Java实验二

---
+++ Java实验/java实验二/hospital/src/sample/Login.java
@@ -0,0 +1,16 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ Java实验/java实验二/hospital/src/sample/LoginController.java
@@ -0,0 +1,4 @@
+package sample;
+
+public class LoginController {
+}
