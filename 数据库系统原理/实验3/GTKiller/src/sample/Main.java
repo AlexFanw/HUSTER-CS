cd62450d07083c52abdd130c2b769155f276dfc2
AlexFanw <alex8802732@gmail.com>
2020-06-14 01:24:09 +0800

    🚀 数据库实验报告

--- 数据库系统原理/实验3/GTKiller/src/sample/Main.java
+++ 数据库系统原理/实验3/GTKiller/src/sample/Main.java
@@ -1,10 +1,10 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.SplitPane;
-import javafx.stage.Stage;
+        import javafx.application.Application;
+        import javafx.fxml.FXMLLoader;
+        import javafx.scene.Scene;
+        import javafx.scene.control.SplitPane;
+        import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
